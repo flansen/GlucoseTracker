813fb0a36210c91688a4b5ff00a8c7bfb9a2f82e
Florian <floh.pg@gmail.com>
2016-10-03 12:31:31 +0200

    init

--- app/src/main/java/de/fha/bwi50101/OverviewActivity.java
+++ app/src/main/java/de/fha/bwi50101/OverviewActivity.java
@@ -2,20 +2,25 @@ package de.fha.bwi50101;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class OverviewActivity extends AppCompatActivity {
-    @BindView(R.id.test)
-    Button test;
 
+public class OverviewActivity extends AppCompatActivity {
+    @BindView(R.id.overview_toolbar)
+    Toolbar toolbar;
+    @BindString(R.string.action_bar_title_overview)
+    String actionBarTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
         ButterKnife.bind(this);
-        new TestInteractor().execute();
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(actionBarTitle);
     }
 }
